a9b109d0e912fe07420a60fdc5d4fef568276458
K.Marvin <kareramarvin14@gmail.com>
2022-11-19 12:06:46 +0200

    first commit

---
+++ src/test/java/com/example/prodtesting/ProdTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prodtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
